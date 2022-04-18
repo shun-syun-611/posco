6ae1e9a7280c67dab2ac34e24f7f221b621f5dda
Shun Sugawara <82685781shun-syun-611@users.noreply.github.com>
2022-04-18 21:04:45 +0900

    claen: material uiのコンポーネントでデザイン適用
    header、PostArea、TimeLineのデザインをmaterial uiにて書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,78 @@
-import React, { useState, useCallback } from 'react';
-import './reset.css'
-import classes from './App.module.scss'
-import PostArea from './components/PostArea/PostArea';
-import TimeLineArea from './components/TimeLineArea/TimeLineArea';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import image from './img/user.jpeg';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from './redux/store';
-import { addTodoAction, addFavoritesAction } from './redux/Todos/actions';
+import React, { useState, useCallback } from "react";
+import "./reset.css";
+import classes from "./App.module.scss";
+import PostArea from "./components/PostArea/PostArea";
+import TimeLineArea from "./components/TimeLineArea/TimeLineArea";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import image from "./img/user.jpeg";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "./redux/store";
+import { addTodoAction, addFavoritesAction } from "./redux/Todos/actions";
 
 function App() {
-
   // reduxを用いた状態管理
-  const todosState = useSelector( (state:RootState) => state.todos )
+  const todosState = useSelector((state: RootState) => state.todos);
   console.log(todosState.posts);
 
-  
   const dispatch = useDispatch();
 
   const addTodos = () => {
     if (inputText !== "") {
-      dispatch(addTodoAction(inputText, fileUrl))
+      dispatch(addTodoAction(inputText, fileUrl));
       setInputText("");
       setFileUrl("");
+    } else {
+      alert("input something");
     }
-  }
+  };
 
-   const addFavorites = (id:number, favorites:number) => {
-      dispatch(addFavoritesAction(id, favorites))
-  }
-
+  const addFavorites = (id: number, favorites: number) => {
+    dispatch(addFavoritesAction(id, favorites));
+  };
 
   // reduxを用いた状態管理 ここまで
 
+  // 投稿内容の入力
+  const [inputText, setInputText] = useState("");
 
-// 投稿内容の入力
-const [inputText, setInputText] = useState("");
+  console.log("通過して初期化されてる");
+  const handleInputText = useCallback(
+    (
+      e:
+        | React.ChangeEvent<HTMLTextAreaElement>
+        | React.ChangeEvent<HTMLInputElement>
+    ) => {
+      setInputText(e.target.value);
+    },
+    [setInputText]
+  );
 
-console.log("通過して初期化されてる");
-const handleInputText = useCallback((e: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>) => {
-  setInputText(e.target.value);
-}, [setInputText]);
+  // 投稿画像のプレビューの出力
+  const [fileUrl, setFileUrl] = useState<string>("");
 
-// 投稿画像のプレビューの出力
-const [fileUrl, setFileUrl] = useState<string>("");
-const previewImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-  const imageFile = e.target.files![0];
-  const imageUrl = URL.createObjectURL(imageFile);
-  setFileUrl(imageUrl);
-  // 同じ画像を選択してもイベントが発火するために最後にvalueを空にする
-  e.target.value = "";
-}
+  const previewImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const imageFile = e.target.files![0];
+    const imageUrl = URL.createObjectURL(imageFile);
+    setFileUrl(imageUrl);
+    // 同じ画像を選択してもイベントが発火するために最後にvalueを空にする
+    e.target.value = "";
+  };
 
-// 投稿画像の選択取消
-const resetSelectedFile = (e: React.MouseEvent<HTMLButtonElement>) => {
-  e.preventDefault();
-  setInputText('');
-  setFileUrl('');
-}
+  // 投稿画像の選択取消
+  const resetSelectedFile = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setInputText("");
+    setFileUrl("");
+  };
 
   return (
     <div className={classes.app}>
-
       <Header />
 
       <main className={classes.main}>
-
         {/* 投稿エリア */}
-        <PostArea 
+        <PostArea
           handleInputText={handleInputText}
           inputText={inputText}
           previewImage={previewImage}
@@ -79,16 +83,14 @@ const resetSelectedFile = (e: React.MouseEvent<HTMLButtonElement>) => {
         />
 
         {/* タイムラインエリア */}
-       <TimeLineArea 
-        image={image}
-        todosState={todosState}
-        addFavorites={addFavorites}
+        <TimeLineArea
+          image={image}
+          todosState={todosState}
+          addFavorites={addFavorites}
         />
-
       </main>
 
       <Footer />
-      
     </div>
   );
 }
--- src/components/PostArea/PostArea.tsx
+++ src/components/PostArea/PostArea.tsx
@@ -1,57 +1,97 @@
-import React, {memo, useContext} from 'react'
-import classes from './PostArea.module.scss'
-import { Button } from '@material-ui/core'
+import React, { memo } from "react";
+import classes from "./PostArea.module.scss";
+import { Button, styled } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
 
+const Input = styled("input")({
+  display: "none",
+});
 
 type Props = {
-  handleInputText: (e: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>) => void;
+  handleInputText: (
+    e:
+      | React.ChangeEvent<HTMLTextAreaElement>
+      | React.ChangeEvent<HTMLInputElement>
+  ) => void;
   inputText: string;
   previewImage: (e: React.ChangeEvent<HTMLInputElement>) => void;
   resetSelectedFile: (e: React.MouseEvent<HTMLButtonElement>) => void;
   fileUrl: string;
   image: string;
   addTodos: () => void;
-}
+};
 
-const PostArea:React.VFC<Props> = memo((props) => {
-
-  const {handleInputText ,inputText ,previewImage ,resetSelectedFile,fileUrl ,image,addTodos} = props;
+const PostArea: React.VFC<Props> = memo((props) => {
+  const {
+    handleInputText,
+    inputText,
+    previewImage,
+    resetSelectedFile,
+    fileUrl,
+    image,
+    addTodos,
+  } = props;
   console.log("PostArea通過");
 
   return (
-   <div className={classes.postArea}>
-            <form className={classes.inputAndPostArea}>
-                <div className={classes.userAndInputArea}>
-                    <div className={classes.userArea}>
-                        <img src={image} className={classes.userImage} alt=""/>
-                        <p className={classes.userName}>user name</p>
-                    </div>
-                    <div className={classes.inputTextArea}>
-                        <textarea onChange={handleInputText} className={classes.inputText} value={inputText}/>
-                    </div>
-                </div>
-                <div className={classes.postArea}>
-                    <div className={classes.inputFileArea}>
-                        <label className={classes.inputFileLabel}>
-                          <input className={classes.inputFile} type="file" accept="image/*" onChange={previewImage}/>
-                            ファイル添付
-                        </label>
-                        {/* ここは、ファイル添付の状態によって、出しわける予定 */}
-                        <span className={classes.inputFileAttention}>選択されていません</span>
-                        <button className={classes.resetButton} onClick={resetSelectedFile}>リセット</button>
-                        <div className={classes.inputFilePreview}>
-                          <img className={classes.inputFileImage} alt="" src={fileUrl} />
-                        </div>
-                    </div>
-                    {/* <div className={classes.postBottunArea}>
-                      <button onClick={postInputText} className={classes.postBottun}>POST</button>
-                    </div> */}
-                     <Button variant="contained" onClick={() => addTodos()}>POST</Button>
-                </div>
-            </form>
+    <div className={classes.postArea}>
+      <form className={classes.inputAndPostArea}>
+        <div className={classes.userAndInputArea}>
+          <div className={classes.userArea}>
+            <img src={image} className={classes.userImage} alt="" />
+            <p className={classes.userName}>user name</p>
+          </div>
+          <div className={classes.inputTextArea}>
+            <TextField
+              id="outlined-multiline-flexible"
+              label="post your content"
+              multiline
+              maxRows={4}
+              onChange={handleInputText}
+              className={classes.inputText}
+              value={inputText}
+            />
+          </div>
         </div>
-  )
+        <div className={classes.postArea}>
+          <div className={classes.inputFileArea}>
+            <div className={classes.inputFileButtonArea}>
+              {fileUrl === "" ? (
+                <label htmlFor="contained-button-file">
+                  <Input
+                    accept="image/*"
+                    id="contained-button-file"
+                    multiple
+                    type="file"
+                    onChange={previewImage}
+                  />
+                  <Button variant="contained" component="span">
+                    Upload
+                  </Button>
+                </label>
+              ) : (
+                <Button
+                  variant="contained"
+                  size="small"
+                  onClick={resetSelectedFile}
+                >
+                  RESET
+                </Button>
+              )}
+            </div>
+          </div>
+
+          <Button variant="contained" onClick={() => addTodos()}>
+            POST
+          </Button>
+        </div>
+
+        <div className={classes.inputFilePreview}>
+          <img className={classes.inputFileImage} alt="" src={fileUrl} />
+        </div>
+      </form>
+    </div>
+  );
 });
 
-export default PostArea
-
+export default PostArea;
